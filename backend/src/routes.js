853bcb8e9ec87768ded25afb3d5034ef5c384bbc
Vitor Morelli <vitao.miacri@gmail.com>
2019-08-06 16:34:26 -0300

    backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
+const DislikeController = require("./controllers/DislikeController");
+
 const routes = express.Router();
 
-server.get("/", (req, res) => {
+routes.get("/", (req, res) => {
 	return res.send("Hello world");
 });
 
-module.exports = {
-	routes
-};
+routes.post("/devs", DevController.store);
+routes.get("/devs", DevController.index);
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DislikeController.store);
+
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const server = express();
+mongoose.connect(
+  "mongodb+srv://semana:semana@cluster0-xiikz.mongodb.net/tindev?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
 
+server.use(cors());
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
